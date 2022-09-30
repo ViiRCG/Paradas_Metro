36bb70b36c190d9f3e9a6e34aee75480cac912e4
Virginia Chico <virgi_13_94@hotmail.com>
2022-09-30 20:13:28 +0200

    Modificación 30/09/2022

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const imagenes = ["alexanderplatz.jpeg", "hermannplatz.jpeg", "jungfernheide.jpe
     "konstanzer.jpeg", "kurfurstenstrabe.jpeg", "paulsternstrasse.jpeg",
     "unterdenlinden.jpeg", "westhafen.jpeg"];
 
+let imgs=document.querySelectorAll(".main__img");
 
 let img1 = document.getElementById("img1")
 let img2 = document.getElementById("img2")
@@ -31,15 +32,26 @@ let info=document.getElementById("info");
 
 // main.addEventListener("click", mostrar)
 
+//SIN USAR EVENT, solo aparece el texto de la imagen en la que hago click
+const mostrar=(img)=>{
+    
+    let pos= Math.floor(Math.random()*8);
 
-const mostrar=()=>{
-    info.textContent=img1.getAttribute("src");
+    info.textContent=img.src.slice(img.src.lastIndexOf("/")+1, img.src.lastIndexOf("."));
+    
+    
 }
 
 
-img1.addEventListener("click", mostrar);
+img1.addEventListener("click", ()=> mostrar(img1));
+img2.addEventListener("click", ()=> mostrar(img2));
+img3.addEventListener("click", ()=> mostrar(img3));
+img4.addEventListener("click", ()=> mostrar(img4));
+img5.addEventListener("click", ()=> mostrar(img5));
+img6.addEventListener("click", ()=> mostrar(img6));
+img7.addEventListener("click", ()=> mostrar(img7));
+img8.addEventListener("click", ()=> mostrar(img8));
 
 
-
-
+/** con una función de flecha metemos el código de la función, por lo que no se va a ejecutar hasta que se produzca el evento click*/
 
