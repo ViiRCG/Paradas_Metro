f4184347d88309b4931c01977daf1286b1401054
Virginia Chico <virgi_13_94@hotmail.com>
2022-09-30 20:31:35 +0200

    Terminado

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const imagenes = ["alexanderplatz.jpeg", "hermannplatz.jpeg", "jungfernheide.jpe
     "konstanzer.jpeg", "kurfurstenstrabe.jpeg", "paulsternstrasse.jpeg",
     "unterdenlinden.jpeg", "westhafen.jpeg"];
 
-let imgs=document.querySelectorAll(".main__img");
+let imgs = document.querySelectorAll(".main__img");
 
 let img1 = document.getElementById("img1")
 let img2 = document.getElementById("img2")
@@ -15,15 +15,17 @@ let img8 = document.getElementById("img8")
 
 /*Tengo que indicar la ruta de las imagenes para que sepa donde encontrarlo. Por eso ponemos "imagenes/" justo antes. Si esa carpeta de imagenes estuviera dentro de una carpeta assets, pondríamos "assets/imagenes/" */
 
-let main=document.getElementById("main");
-let info=document.getElementById("info");
+//let main=document.getElementById("main");
 
+let info = document.getElementById("info");
+
+//Esta parte del event es la que dimos en primero:
 
 // const mostrar = (event) => {
 //     let pos;
 
 //     if(event.target.tagName == "IMG"){
-       
+
 //         pos=Math.floor(Math.random()*imagenes.length)
 //         event.target.src="imagenes/" + imagenes[pos]; 
 //         console.log(event.target)
@@ -32,25 +34,30 @@ let info=document.getElementById("info");
 
 // main.addEventListener("click", mostrar)
 
-//SIN USAR EVENT, solo aparece el texto de la imagen en la que hago click
-const mostrar=(img)=>{
-    
-    let pos= Math.floor(Math.random()*8);
+//Esta parte la hemos dado en segundo. SIN USAR EVENT.
+const mostrar = (img) => {
+    let pos;
 
-    info.textContent=img.src.slice(img.src.lastIndexOf("/")+1, img.src.lastIndexOf("."));
-    
-    
+    do {
+        pos = Math.floor(Math.random() * imagenes.length);
+    } while (imagenes[pos] == img.src.slice(img.src.lastIndexOf("/")+1));
+
+    img.src = "imagenes/" + imagenes[pos];
+
+    info.textContent = img.src.slice(img.src.lastIndexOf("/") + 1, img.src.lastIndexOf("."));
+
+
 }
 
 
-img1.addEventListener("click", ()=> mostrar(img1));
-img2.addEventListener("click", ()=> mostrar(img2));
-img3.addEventListener("click", ()=> mostrar(img3));
-img4.addEventListener("click", ()=> mostrar(img4));
-img5.addEventListener("click", ()=> mostrar(img5));
-img6.addEventListener("click", ()=> mostrar(img6));
-img7.addEventListener("click", ()=> mostrar(img7));
-img8.addEventListener("click", ()=> mostrar(img8));
+img1.addEventListener("click", () => mostrar(img1));
+img2.addEventListener("click", () => mostrar(img2));
+img3.addEventListener("click", () => mostrar(img3));
+img4.addEventListener("click", () => mostrar(img4));
+img5.addEventListener("click", () => mostrar(img5));
+img6.addEventListener("click", () => mostrar(img6));
+img7.addEventListener("click", () => mostrar(img7));
+img8.addEventListener("click", () => mostrar(img8));
 
 
 /** con una función de flecha metemos el código de la función, por lo que no se va a ejecutar hasta que se produzca el evento click*/
